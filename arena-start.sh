d2e978e38317612508fd621681701e230f42bfd5
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-03-06 15:01:06 +0200

    Update arena-start.sh

--- arena-start.sh
+++ arena-start.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 PID_NODE="$(pidof node)"
+PID_CH="$(pidof chromium-browser)"
 awhile=3
 if [  "$PID_NODE" != ""  ]; then
   kill $PID_NODE
-else
-  sleep $awhile && chromium-browser -kiosk http://localhost:8080/pagetest.html & 
-  node nodetest.js &
+  while kill -0 $PID_NODE; do 
+    sleep 1
+  done
 fi
+if [  "$PID_CH" != ""  ]; then
+  kill $PID_CH
+  while kill -0 $PID_CH; do 
+    sleep 1
+  done
+fi
+sleep $awhile && chromium-browser -kiosk http://localhost:8080/pagetest.html & 
+node nodetest.js &
