ea8086f0ef16b6ead1001bdc186880a7905d465b
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-03-06 19:41:43 +0200

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -30,7 +30,7 @@ http.createServer(function (req, res) {
 
 function blinkLED() { //function to start blinking
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
+    LED.writeSync(0); //set pin state to 1 (turn LED on)
   } else {
     LED.writeSync(0); //set pin state to 0 (turn LED off)
   }
