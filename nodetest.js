84977f77de08fc3787f28d47bf98c8cba68a1dca
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-04-21 13:46:40 +0300

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -4,6 +4,15 @@ var url = require('url');
 var fs = require('fs');
 var st = 0;
 var LED = new Gpio(27, 'out'); //use GPIO pin 4, and specify that it is output
+var SerialPort = require('serialport').SerialPort;
+var portName = '/dev/ttyACM0';
+var port = new SerialPort(portName, {
+   baudRate: 9600,
+   dataBits: 8,
+   parity: 'none',
+   stopBits: 1,
+   flowControl: false
+});
 var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
 //var pushButton = new Gpio(4, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
@@ -11,6 +20,25 @@ var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function eve
 //   res.writeHead(200, {'Content-Type': 'text/html'});
 //   res.end('Hello World!');
 // }).listen(8080);
+port.write('main screen turn on', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
+// Read data that is available but keep the stream in "paused mode"
+port.on('readable', function () {
+  console.log('Data:', port.read())
+})
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log('Data:', data)
+})
+//const lineStream = port.pipe(new Readline())
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
