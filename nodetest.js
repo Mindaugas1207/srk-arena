9f7d9bb6274f2fefe966275f17d40a2acf94346e
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-04-23 21:56:03 +0300

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -1,9 +1,7 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var st = 0;
-var LED = new Gpio(27, 'out'); //use GPIO pin 4, and specify that it is output
 const { SerialPort } = require('serialport');
 
 // Create a port
@@ -12,8 +10,6 @@ const port = new SerialPort({
   baudRate: 9600,
 });
 var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
-//var pushButton = new Gpio(4, 'in', 'both');
-
 
 function sprint(str) {
     port.write(str, function(err) {
@@ -22,6 +18,13 @@ function sprint(str) {
       }
     })
 }
+
+function leds_set(clr_str) {
+  var str = "SET:LED(" + clr_str + ")\n";
+  sprint(str);
+  console.log('wr>', str)
+}
+
 // Open errors will be emitted as an error event
 port.on('error', function(err) {
   console.log('Error: ', err.message)
@@ -53,35 +56,16 @@ http.createServer(function (req, res) {
 
 function blinkLED() { //function to start blinking
   if (st === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
+    leds_set("FF,00,00;00,FF,00;00,00,FF");
     st = 1;
   } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
+    leds_set("00,00,00;00,00,00;00,00,00");
     st = 0;
   }
-  sprint('hello');
 }
 
 function endBlink() { //function to stop blinking
   clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
 }
 
-setTimeout(endBlink, 10000); //stop blinking after 5 seconds
-
-// pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-//   if (err) { //if an error
-//     console.error('There was an error', err); //output error message to console
-//   return;
-//   }
-//   LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
-// });
-
-// function unexportOnClose() { //function to run when exiting program
-//   LED.writeSync(0); // Turn LED off
-//   LED.unexport(); // Unexport LED GPIO to free resources
-//   pushButton.unexport(); // Unexport Button GPIO to free resources
-// };
-
-// process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+setTimeout(endBlink, 20000); //stop blinking after 5 seconds
