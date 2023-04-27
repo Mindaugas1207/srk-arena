611946df9b8195dee7687b51df8c3cb298c798ab
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-04-27 16:24:06 +0300

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -19,6 +19,13 @@ const port = new SerialPort({
 var t_seconds = 0;
 var t_minutes = 0;
 
+var t_start = new Date().getTime();
+var t_period = 0;
+var t_remaining = 0;
+var sys_timer_end = false;
+var sys_timer_pause = false;
+var sys_timer_enabled = false;
+
 var blinkInterval;
 var blinkTimeout;
 const SYS_START_NO = 0;
@@ -36,34 +43,49 @@ var sys_right_door_open = false;
 var sys_left_door_open = false;
 
 const ARRIVAL_TIME = 5; //min
-const ARRIVAL_RUNOFF_TIME = 500; //ms
 const SYS_ARRIVAL_NO = 0;
 const SYS_ARRIVAL_START = 1;
-const SYS_ARRIVAL_RUN = 2;
-const SYS_ARRIVAL_END = 3;
+const SYS_ARRIVAL_END = 2;
 var sys_arrival_state = SYS_ARRIVAL_NO;
 var sys_arrival_compA = false;
 var sys_arrival_compB = false;
 var sys_arrival_interval;
 
 const PREP_TIME = 2; //min
-const PREP_RUNOFF_TIME = 500; //ms
 const SYS_PREP_NO = 0;
 const SYS_PREP_START = 1;
-const SYS_PREP_RUN = 2;
-const SYS_PREP_END = 3;
-var sys_prep_state = SYS_ARRIVAL_NO;
+const SYS_PREP_END = 2;
+var sys_prep_state = SYS_PREP_NO;
 var sys_prep_compA = false;
 var sys_prep_compB = false;
 var sys_prep_interval;
+
+const MATCH_TIME = 5; //min
+const SYS_MATCH_NO = 0;
+const SYS_MATCH_START = 1;
+const SYS_MATCH_END = 2;
+var sys_match_state = SYS_MATCH_NO;
+
+var sys_match_num = 0;
+var sys_round_num = 0;
+var sys_compA_name = "Dalyvis A";
+var sys_compB_name = "Dalyvis B";
+var sys_compA_pnts = 0;
+var sys_compB_pnts = 0;
+var sys_compA_arrived = false;
+var sys_compB_arrived = false;
+var sys_compA_ready = false;
+var sys_compB_ready = false;
 
 const SYS_STATE_INIT = 0;
 const SYS_STATE_START = 1;
 const SYS_STATE_ARRIVAL = 2;
 const SYS_STATE_PREP = 3;
-const SYS_STATE_BATTLE = 4;
+const SYS_STATE_MATCH = 4;
 const SYS_STATE_IDLE = 5;
+const SYS_STATE_DEMO = 6;
 var sys_state = SYS_STATE_INIT;
+var sys_next_state = SYS_STATE_INIT;
 setTimeout(sys_start, 5000);
 
 function sprint(str) {
@@ -77,19 +99,16 @@ function sprint(str) {
 function leds_set(clr1_str, clr2_str, clr3_str) {
   var str = "SET:LED(" + clr1_str + ";" + clr2_str + ";" + clr3_str + ")\n";
   sprint(str);
-  //console.log('wr>', str)
 }
 
 function ramp_set(time_str) {
   var str = "SET:RMP(" + time_str + ")\n";
   sprint(str);
-  //console.log('wr>', str)
 }
 
 function blade_set(time_str, rev_str, auto_str, auto_time_str) {
   var str = "SET:SPN(" + time_str + "," + rev_str + "," + auto_str + "," + auto_time_str + ")\n";
   sprint(str);
-  //console.log('wr>', str)
 }
 
 // Open errors will be emitted as an error event
@@ -118,6 +137,7 @@ port.on('readable', function () {
     if (sys_start_state === SYS_START_PENDING)
     {
       sys_start_state = SYS_START_OK;
+      console.log('SRV:', "SYS_START_OK");
     }
   }
   else if (newData.startsWith("OK"))
@@ -132,9 +152,6 @@ port.on('readable', function () {
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    
-    console.log('SRV:', q.pathname);
-    
     if (q.pathname == "/data")
     {
       var data = {
@@ -144,20 +161,105 @@ http.createServer(function (req, res) {
         "RD": sys_right_door,
         "LD": sys_left_door,
         "RS": sys_right_sw,
-        "LS": sys_left_sw
+        "LS": sys_left_sw,
+        "MATCH": sys_match_num,
+        "ROUND": sys_round_num,
+        "COMPA": sys_compA_name,
+        "COMPB": sys_compB_name,
+        "PNTA": sys_compA_pnts,
+        "PNTB": sys_compB_pnts,
       }
       res.setHeader("Content-Type", "application/json");
       res.writeHead(200);
       return res.end(JSON.stringify(data, null, 3));
     }
+    else if (q.pathname == "/confirmA")
+    {
+      sys_compA_arrived = true;
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/confirmB")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/cancelA")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/cancelB")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/addA")
+    {
+      sys_compA_pnts += 1;
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/addB")
+    {
+      sys_compB_pnts += 1;
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/subA")
+    {
+      sys_compA_pnts -= 1;
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/subB")
+    {
+      sys_compB_pnts -= 1;
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/confirm")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/cancel")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/next")
+    {
+      sys_sw_state(sys_next_state);
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/back")
+    {
+      res.writeHead(200);
+      return res.end();
+    }
     else if (q.pathname == "/pause")
     {
-      startBlink(500, 20000, "00,00,FF", "00,FF,00", "FF,00,00", "00,00,00", "00,00,00", "00,00,00");
+      pause_time();
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/resume")
+    {
+      resume_time();
       res.writeHead(200);
       return res.end();
     }
     else if (q.pathname == "/stop")
     {
+      cancel_time();
+      res.writeHead(200);
+      return res.end();
+    }
+    else if (q.pathname == "/reset")
+    {
+      reset_time();
       res.writeHead(200);
       return res.end();
     }
@@ -204,28 +306,136 @@ function sys_start()
   sys_state = SYS_STATE_START;
   sys_start_state = SYS_START_PENDING;
   sprint("SYSTEM_START()\n");
-  sys_interval = setInterval(sys_run, 1000);
+  console.log('SRV:', "SYS_START_PENDING");
+  sys_interval = setInterval(sys_run, 500);
 }
 
-// Set the date we're counting down to
-var countStart = new Date().getTime();
-function getElapsed(){
+function start_time(period)
+{
+  sys_timer_enabled = false;
+  t_start = new Date().getTime();
+  t_period = period;
+  t_remaining = t_period;
+  t_minutes = 0;
+  t_seconds = 0;
+  sys_timer_end = false;
+  sys_timer_pause = false;
+  sys_timer_enabled = true;
+}
+
+function pause_time()
+{
+  if (sys_timer_pause == true)
+    return;
+  check_time();
+  sys_timer_pause = true;
+}
+
+function resume_time()
+{
+  if (sys_timer_pause == false)
+    return;
+  check_time();
+  sys_timer_pause = false;
+}
+
+function cancel_time()
+{
+  sys_timer_enabled = false;
+  t_remaining = 0;
+  sys_timer_end = true;
+  sys_timer_pause = false;
+  t_minutes = 0;
+  t_seconds = 0;
+}
+
+function reset_time()
+{
+  t_start = new Date().getTime();
+  t_remaining = t_period;
+  sys_timer_end = false;
+  sys_timer_pause = false;
+  sys_timer_enabled = true;
+}
+
+function run_time()
+{
+  if (sys_timer_enabled == false)
+    return;
+
   var now = new Date().getTime();
-  var distance = now - countStart;
-  t_minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  t_seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if (sys_timer_pause == false && sys_timer_end == false)
+  {
+    t_remaining = t_remaining - (now - t_start);
+  }
+  t_start = now;
+  
+  if (t_remaining <= 0)
+  {
+    t_remaining = 0;
+    sys_timer_end = true;
+    sys_timer_pause = false;
+    t_minutes = 0;
+    t_seconds = 0;
+  }
+  else
+  {
+    t_minutes = Math.floor((t_remaining % (1000 * 60 * 60)) / (1000 * 60));
+    t_seconds = Math.floor((t_remaining % (1000 * 60)) / 1000);
+  }
+}
+
+function check_time()
+{
+  return sys_timer_end;
 }
 
 function sys_run()
 {
+  run_time();
+
   if (sys_state === SYS_STATE_START)
   {
     if (sys_start_state === SYS_START_OK)
     {
-      getElapsed();
+      sys_state === SYS_STATE_IDLE;
     }
   }
-  
+  else if (sys_state === SYS_STATE_ARRIVAL)
+  {
+    arrival_run();
+  }
+  else if (sys_state === SYS_STATE_PREP)
+  {
+    prep_run();
+  }
+  else if (sys_state === SYS_STATE_MATCH)
+  {
+    match_run();
+  }
+  else if (sys_state === SYS_STATE_IDLE)
+  {
+
+  }
+  else if (sys_state === SYS_STATE_DEMO)
+  {
+  }
+}
+
+function sys_sw_state(st)
+{
+  if (st === SYS_STATE_ARRIVAL)
+  {
+    arrival_start();
+  }
+  else if (st === SYS_STATE_PREP)
+  {
+    prep_start();
+  }
+  else if (st === SYS_STATE_MATCH)
+  {
+    match_start();
+  }
 }
 
 function sys_end()
@@ -236,102 +446,110 @@ function sys_end()
 
 function arrival_start()
 {
+  start_time(ARRIVAL_TIME * 60000);
   sys_arrival_state = SYS_ARRIVAL_START;
-  sys_arrival_interval = setTimeout(arrival_run, ARRIVAL_TIME * 60000);
+  sys_state = SYS_STATE_ARRIVAL;
 }
 
 function arrival_run()
 {
-  sys_arrival_state = SYS_ARRIVAL_RUN;
-  sys_arrival_interval = setTimeout(arrival_end, ARRIVAL_RUNOFF_TIME);
-}
+  if (sys_arrival_state === SYS_ARRIVAL_START)
+  {
+    if (check_time() == true)
+    {
+      sys_arrival_state = SYS_ARRIVAL_END;
+      sys_next_state = SYS_STATE_PREP;
+    }
+    else
+    {
 
-function arrival_end()
-{
-  sys_arrival_state = SYS_ARRIVAL_END;
+    }
+  }
 }
 
 function prep_start()
 {
+  start_time(PREP_TIME * 60000);
   sys_prep_state = SYS_PREP_START;
-  sys_prep_interval = setTimeout(prep_run, PREP_TIME * 60000);
+  sys_state = SYS_STATE_PREP;
 }
 
 function prep_run()
 {
-  sys_prep_state = SYS_PREP_RUN;
-  sys_prep_interval = setTimeout(prep_end, PREP_RUNOFF_TIME);
+  if (sys_prep_state === SYS_PREP_START)
+  {
+    if (check_time() == true)
+    {
+      sys_prep_state = SYS_PREP_END;
+      sys_next_state = SYS_STATE_MATCH;
+    }
+    else
+    {
+      
+    }
+  }
 }
 
-function prep_end()
+function match_start()
 {
-  sys_prep_state = SYS_PREP_END;
+  start_time(MATCH_TIME * 60000);
+  sys_match_state = SYS_MATCH_START;
+  sys_state = SYS_STATE_MATCH;
+}
+
+function match_run()
+{
+  if (sys_match_state === SYS_MATCH_START)
+  {
+    if (check_time() == true)
+    {
+      sys_match_state = SYS_MATCH_END;
+      sys_next_state = SYS_STATE_IDLE;
+    }
+    else
+    {
+      
+    }
+  }
 }
 
 function input_update_states()
 {
+  if (sys_right_door == sys_right_door_open || sys_left_door == sys_left_door_open)
+  {
+    sys_right_door_open = !sys_right_door;
+    sys_left_door_open = !sys_left_door;
+
+    if (sys_right_door_open || sys_left_door_open)
+    {
+      pause_time();
+    }
+    else
+    {
+      resume_time();
+    }
+  }
+
   if (sys_state === SYS_STATE_ARRIVAL)
   {
-    if (sys_arrival_state === SYS_ARRIVAL_START || sys_arrival_state === SYS_ARRIVAL_RUN)
-    {
-      sys_arrival_compA = sys_right_sw;
-      sys_arrival_compB = sys_left_sw;
-    }
-    
-    if (sys_arrival_state === SYS_ARRIVAL_START)
-    {
-      if (sys_arrival_compA == true && sys_arrival_compB == true)
-      {
-        clearInterval(sys_arrival_interval);
-        arrival_run();
-      }
-    }
+
   }
   else if (sys_state === SYS_STATE_PREP)
   {
-    if (sys_prep_state === SYS_PREP_START || sys_prep_state === SYS_PREP_RUN)
-    {
-      if (sys_prep_compA == true && sys_right_door == false)
-      {
-        sys_right_door_open = true;
-      }
-      else if (sys_right_door_open == false && sys_right_door == false)
-      {
-        sys_right_door_open = true;
-        sys_prep_compA = false;
-      }
-      else if (sys_right_door_open == true && sys_right_door == true)
-      {
-        sys_right_door_open = false;
-        sys_prep_compA = true;
-      }
 
-      if (sys_prep_compB == true && sys_left_door == false)
-      {
-        sys_left_door_open = true;
-      }
-      else if (sys_left_door_open == false && sys_left_door == false)
-      {
-        sys_left_door_open = true;
-        sys_prep_compB = false;
-      }
-      else if (sys_left_door_open == true && sys_left_door == true)
-      {
-        sys_left_door_open = false;
-        sys_prep_compB = true;
-      }
-    }
-    
-    if (sys_prep_state === SYS_PREP_START)
-    {
-      if (sys_prep_compA == true && sys_prep_compB == true)
-      {
-        clearInterval(sys_prep_interval);
-        prep_run();
-      }
-    }
   }
+  else if (sys_state === SYS_STATE_MATCH)
+  {
 
+  }
+  else if (sys_state === SYS_STATE_IDLE)
+  {
+
+  }
+  else if (sys_state === SYS_STATE_DEMO)
+  {
+
+  }
 }
 
 //blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
