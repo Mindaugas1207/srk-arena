8e8b537711de0edcfe450cffa7787b6cbbb72919
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-04-28 18:24:45 +0300

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -1001,33 +1001,52 @@ function input_update_states()
 }
 
 function api_getMatches(){
-  const postData = querystring.stringify({
-  });
+  http.get('https://'+api_user+':'+api_key+'@api.challonge.com/v1/tournaments/'+api_tournament+'/matches.json', res => {
+    let data = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.statusCode);
+    console.log('Date in Response header:', headerDate);
   
-  const options = {
-    hostname: 'https://'+api_user+':'+api_key+'@api.challonge.com',
-    port: 443,
-    path: '/v1/tournaments/"+api_tournament+"/matches.json',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postData.length
-    }
-  };
-  
-  const req = http.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
-    res.on('data', (d) => {
-      process.stdout.write(d);
+    res.on('data', chunk => {
+      data.push(chunk);
     });
-  });
   
-  req.on('error', (error) => {
-    console.error(error);
+    res.on('end', () => {
+      console.log('Response ended: ');
+      const users = JSON.parse(Buffer.concat(data).toString());
+      console.log(users);
+    });
+  }).on('error', err => {
+    console.log('Error: ', err.message);
   });
+
+  // const postData = querystring.stringify({
+  // });
   
-  req.write(postData);
-  req.end();
+  // const options = {
+  //   hostname: 'https://'+api_user+':'+api_key+'@api.challonge.com',
+  //   port: 443,
+  //   path: '/v1/tournaments/"+api_tournament+"/matches.json',
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //     'Content-Length': postData.length
+  //   }
+  // };
+  
+  // const req = http.request(options, (res) => {
+  //   console.log(`statusCode: ${res.statusCode}`);
+  //   res.on('data', (d) => {
+  //     process.stdout.write(d);
+  //   });
+  // });
+  
+  // req.on('error', (error) => {
+  //   console.error(error);
+  // });
+  
+  // req.write(postData);
+  // req.end();
 
 	// var xh = new XMLHttpRequest();
 	// xh.onreadystatechange = function(){
