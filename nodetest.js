ed38bece3b04d98b28f64f69aa8a2aa4655961d4
Mindaugas1207 <103382650+Mindaugas1207@users.noreply.github.com>
2023-04-29 14:30:00 +0300

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -1187,9 +1187,12 @@ function api_getMatches(){
         }
 
         api_setMatchActive(api_next_match);
-
-        api_getParticipantA(api_next_match);
-        api_getParticipantB(api_next_match);
+        sys_compA_name = "A";
+        api_participantA_ok = true;
+        sys_compB_name = "B";
+        api_participantB_ok = true;
+        //api_getParticipantA(api_next_match);
+        //api_getParticipantB(api_next_match);
       }
       else
       {
